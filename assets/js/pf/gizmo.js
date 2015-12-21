590b97bd01c994442540a4029f7cd8bd6bc03193
Ondřej Želazko <zelazko@insiaoffice.cz>
2015-12-21 12:38:52 +0100

    falling gizmos + collision detection

---
+++ assets/js/pf/gizmo.js
@@ -0,0 +1,59 @@
+
+PF.gizmos = {
+
+    objects: [],
+
+    prepareForTestPlay: function () {
+        PF.scene.add(this.creator);
+        PF.scene.add(this);
+    },
+
+    update: function () {
+        var self = this;
+        this.objects.forEach(function (g, i) {
+            if (g) {
+                g.position.y += g.motion_vector.y;
+                if (g.position.y > PF.canvas.buffer.height - 10) {
+                    self.objects[i] = null;
+                }
+                if (g.hit_circle.collide(PF.player.sprite.hit_circle)) {
+                    self.objects[i] = null;
+                }
+            }
+        });
+    },
+
+    render: function (ctx) {
+        this.objects.forEach(function (g) {
+            if (g) g.render(ctx);
+        });
+    },
+
+    getNextEmptyPosition: function () {
+        var i;
+        for (i=0; i<this.objects.length; i+=1) {
+            if (!this.objects[i]) return i;
+        }
+        return this.objects.length;
+    },
+
+    creator: new D2O.Looper(1, function () {
+        var g = new D2O.Sprite(PF.images["gizmo"]);
+        g.position.x = Math.random() * PF.canvas.buffer.width;
+        g.motion_vector.y = 0.5;
+
+        g.hit_circle = new D2O.Sprite.HitCircle(g, g.calcInnerRadius());
+        g.hit_circle.stroke_style = "green";
+        g.render_more = function (ctx) {
+            g.hit_circle.render(ctx);
+        };
+
+        PF.gizmos.objects[PF.gizmos.getNextEmptyPosition()] = g;
+    })
+
+
+
+
+
+
+};
--- assets/js/pf/player.js
+++ assets/js/pf/player.js
@@ -1,18 +1,75 @@
 
 PF.player = {
 
+    move_speed: 10,
+
+    go_left: false,
+    //dragg_left: false,
+    go_right: false,
+    //dragg_right: false,
+
     prepareForTestPlay: function () {
-        this.sprite = new D2O.Sprite(PF.images["player"]);
-        this.sprite.position.set(25, 70);
+        this.createSprite();
+        this.gameplay = true;
+        PF.scene.add(this.sprite);
+    },
 
+    createSprite: function () {
+        var p = this, sprite = new D2O.Sprite(PF.images["player"]);
+        var half_width = sprite.width / 2;
+
+        sprite.position.set(25, 70);
+
+        sprite.update = function (delta) {
+            var x_motion = 0;
+            if (p.go_left) {
+            //if (p.dragg_left || p.go_left) {
+                x_motion -= p.move_speed;
+                //p.dragg_left = false;
+            }
+            if (p.go_right) {
+            //if (p.dragg_right || p.go_right) {
+                x_motion += p.move_speed;
+                //p.dragg_right = false;
+            }
+
+            this.position.x += x_motion * delta;
+            if (this.position.x < half_width) {
+                this.position.x = half_width;
+            }
+            if (this.position.x > PF.canvas.buffer.width - half_width) {
+                this.position.x = PF.canvas.buffer.width - half_width;
+            }
+
+            //p.dragg_left = p.go_left;
+            //p.dragg_right = p.go_right;
+        };
+
+        sprite.hit_circle = new D2O.Sprite.HitCircle(sprite, sprite.width/2,
+            new D2O.Vector2(0, -sprite.height/3.2));
+        sprite.hit_circle.stroke_style = "yellow";
+        sprite.render_more = function (ctx) {
+            sprite.hit_circle.render(ctx);
+        };
+
+        this.sprite = sprite;
     }
-
 
 };
 
 document.addEventListener("keydown", function (e) {
-
+    if (!PF.player.gameplay) return;
+    if (e.keyCode===37) {
+        PF.player.go_left = true;
+        //PF.player.dragg_left = true;
+    }
+    if (e.keyCode===39) {
+        PF.player.go_right = true;
+        //PF.player.dragg_right = true;
+    }
 });
 document.addEventListener("keyup", function (e) {
-
+    if (!PF.player.gameplay) return;
+    if (e.keyCode===37) PF.player.go_left = false;
+    if (e.keyCode===39) PF.player.go_right = false;
 });
